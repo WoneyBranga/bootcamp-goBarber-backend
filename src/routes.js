eda6b8d3fa58dda9d5e1bd6d1710f9e70ff297bd
woneybranga <woney.branga@gmail.com>
2019-07-09 10:07:16 -0300

    configs eslint, prettier e editorconfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import Router from "express";
+import Router from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  res.json({ message: "Hello RocketSeat" });
+routes.get('/', (req, res) => {
+  res.json({ message: 'Hello RocketSeat' });
 });
 
 export default routes;
