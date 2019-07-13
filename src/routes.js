2618c96372583706bd73ddca8da7689cb75e0a59
woneybranga <woney.branga@gmail.com>
2019-07-13 11:04:23 -0300

    correcao de validacao da rota post_sessions

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.post('/sessions', authMiddleware, SessionController.store);
+routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
