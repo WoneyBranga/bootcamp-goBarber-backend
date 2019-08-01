ad306c80412617f64d0824f30aab930b831f4c6c
woneybranga <woney.branga@gmail.com>
2019-08-01 10:42:17 -0300

    mod03.15 Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8bdce82f6a3944',
+    pass: 'fbd335232b0e16',
+  },
+  default: {
+    from: 'Go barber <noreply@gobarber.com>',
+  },
+};
